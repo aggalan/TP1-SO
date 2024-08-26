3bbb7cc3e910de9d21cc215bc3c04f8905fda7db
aggalan <aggalan@itba.edu.ar>
2024-08-26 14:54:34 -0300

    add slave logic

--- slave.c
+++ slave.c
@@ -1,2 +1,38 @@
 #include "commons.h"
+#define MAX_CMD_SIZE 100
+#define MAX_PATH_SIZE 100
 
+int main() {
+    
+    char path[MAX_PATH_SIZE] = {0};
+    char command[MAX_CMD_SIZE];
+    char * md5 = "md5sum %s";
+
+    while(1){
+
+    // lectura del pipe (HACER)
+
+    //Me fijo que queden path para leer, sino corto
+    if(path[0] ==  0){
+        break;
+    }
+    
+    //Guardo el comando 
+    sprintf(command, md5, path);
+
+    //Ejecuto el comando en la shell
+    FILE * log = popen(command, "r");
+
+    if(log == NULL){
+        perror("peopen");
+        exit(EXIT_FAILURE);
+    }
+
+    pclose(log);
+
+    }
+
+    // Escribir el pipe
+
+    return 0;
+}
