d6b463af25c8faefd0e4e3d39e8a93909159824b
aggalan <aggalan@itba.edu.ar>
2024-08-26 13:15:18 -0300

    include commons

---
+++ commons.h
@@ -0,0 +1,9 @@
+#ifndef _COMMONS
+#define _COMMONS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif
--- md5.c
+++ md5.c
@@ -1,1 +1,12 @@
+#include "commons.h"
 
+int main(int argc, char *argv[]) {
+    
+    if(argc < 2){
+        fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+
+    return 0;
+}
