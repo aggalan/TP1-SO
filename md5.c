6fb7301097677f3968eb348191e7cf9bb9ca9f8b
smaffeo33 <smaffeo@itba.edu.ar>
2024-08-27 21:43:15 -0300

    changes things makes it work but shouldnt

--- md5.c
+++ md5.c
@@ -1,109 +1,93 @@
 #include "commons.h"
 #include <sys/wait.h>
+#include <sys/select.h>
+#include <errno.h>
 
-#define MAX_SLAVES 2
 #define FILES_PER_SLAVE 1
 
-int main(int argc, char *argv[]) {
-    
-    if(argc < 2){
+int main(int argc, const char *argv[]) {
+    if (argc < 2) {
         fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    char *file_paths[argc - 1];
+    int slaves = ((argc - 1) / 20) > 0 ? (argc - 1) / 20 : 1;
     int files_processed = 0;
     int max_fd = 0;
 
-    const char * filename = "results.txt";
-    FILE * file = fopen(filename, "w");
+    const char *filename = "results.txt";
+    FILE *file = fopen(filename, "w");
 
     if (file == NULL) {
         perror("Error opening file");
         return EXIT_FAILURE;
     }
 
+    int pipe_to_child[slaves][2], pipe_from_child[slaves][2];
+    pid_t pids[slaves];
 
-    // tengo que leer y escribir con los hijos asi que creo dos pipes
-    int pipe_to_child[MAX_SLAVES][2], pipe_from_child[MAX_SLAVES][2]; 
-
-    pid_t pids[MAX_SLAVES];
-
-    for (int i = 0; i< MAX_SLAVES; i++) {
+    for (int i = 0; i < slaves; i++) {
         if (pipe(pipe_to_child[i]) == -1 || pipe(pipe_from_child[i]) == -1) {
             perror("pipe");
             exit(EXIT_FAILURE);
         }
 
         if ((pids[i] = fork()) == 0) {
-            //proceso hijo
+            // Child process
             close(pipe_to_child[i][1]);
             close(pipe_from_child[i][0]);
 
-            //redirecciono las entradas y salidas correspondiente de los pipes
             dup2(pipe_to_child[i][0], STDIN_FILENO);
             close(pipe_to_child[i][0]);
-            
 
-            dup2(pipe_from_child[i][1], STDOUT_FILENO);
+            //dup2(pipe_from_child[i][1], STDOUT_FILENO);
             close(pipe_from_child[i][1]);
 
-            //ver tema argumentos
             char *args[] = {"./slave", NULL};
             execve(args[0], args, NULL);
             perror("execve");
             exit(EXIT_FAILURE);
-
         }
-        //proceso padre
-        close(pipe_to_child[i][0]); //cierro el extreo de lectura
-        close(pipe_from_child[i][1]); //cierro el extremo de escritura 
+        // Parent process
+       close(pipe_to_child[i][0]); // Close read end
+
+       
+        if (pipe_to_child[i][1] > max_fd) {
+            max_fd = pipe_to_child[i][1];
+        }
+        if (pipe_from_child[i][0] > max_fd) {
+            max_fd = pipe_from_child[i][0];
+        }
     }
 
-    fd_set read_fds, write_fds; //armo mis 2 fd sets para el select (uno para cada pipe)
-    struct timeval timeout; // ni idea el select recibe este struct pero detalles
+    fd_set read_fds, write_fds;
 
     while (files_processed < argc - 1) {
-        //inicializo ambos sets para que esten vacios
         FD_ZERO(&read_fds);
         FD_ZERO(&write_fds);
 
-
-        for (int i = 0; i < MAX_SLAVES; i++) {
-            
-            //meto todos los fds en los sets correspondientes
-            FD_SET(pipe_to_child[i][1], &write_fds); 
-            FD_SET(pipe_from_child[i][0], &read_fds); 
-
-            //calculo el maximo para select
-            if (pipe_to_child[i][1] > max_fd) {
-                max_fd = pipe_to_child[i][1];
-            }
-            if (pipe_from_child[i][0] > max_fd) {
-                max_fd = pipe_from_child[i][0];
-            }
+        for (int i = 0; i < slaves; i++) {
+            FD_SET(pipe_to_child[i][1], &write_fds);
+            FD_SET(pipe_from_child[i][0], &read_fds);
         }
 
-        timeout.tv_sec = 1; // nuevamente, ni idea le clavo un segundo juan carlos me dijo
-        timeout.tv_usec = 0;
+        int activity = select(max_fd + 1, &read_fds, &write_fds, NULL, NULL);
 
-        //el famoso select, que borra de los sets todos los fds que esten blocked y deja solo los ready
-        int activity = select(max_fd + 1, &read_fds, &write_fds, NULL, &timeout); 
-
-        //standard issue chequeo de error
         if (activity < 0) {
             perror("select");
             exit(EXIT_FAILURE);
         }
 
-
-        for (int i = 0; i < MAX_SLAVES; i++) {
+        for (int i = 0; i < slaves; i++) {
             if (FD_ISSET(pipe_to_child[i][1], &write_fds)) {
-                //esta disponible el pipe para pasarle cositas al hijo
-                if (files_processed < argc - 1) { //confirmar si es necesario? no creo pero no mata a nadie asi q lo dejo
+                if (files_processed < argc - 1) {
                     ssize_t bytes_written = write(pipe_to_child[i][1], argv[files_processed + 1], strlen(argv[files_processed + 1]) + 1);
-                    //standard issue chequeo de error
                     if (bytes_written == -1) {
+                        if (errno == EPIPE) {
+                            // Handle broken pipe error
+                            fprintf(stderr, "Broken pipe detected\n");
+                            continue;
+                        }
                         perror("write");
                         exit(EXIT_FAILURE);
                     }
@@ -112,50 +96,50 @@ int main(int argc, char *argv[]) {
             }
 
             if (FD_ISSET(pipe_from_child[i][0], &read_fds)) {
-                //esta listo el pipe para ser leido
                 char buffer[256];
-                ssize_t bytes_read = read(pipe_from_child[i][0], buffer, sizeof(buffer));
+                ssize_t bytes_read = read(pipe_from_child[i][0], buffer, sizeof(buffer) - 1);
                 if (bytes_read > 0) {
-                    buffer[bytes_read-1] = '\n'; // quiero que haga enter >:(
-                    buffer[bytes_read] = '\0'; // null terinated
-                    // printf("recibido de hijo %d: %s\n", i, buffer); lol no va
-                    if (fprintf(file, "%s", buffer) < 0) {// escribo en el archivo results un md5
+                    // Ensure null-termination
+                    buffer[bytes_read] = '\0';
+                    if (fprintf(file, "%s\n", buffer) < 0) {
                         perror("Error writing to file");
                         fclose(file);
                         exit(EXIT_FAILURE);
                     }
 
-                    // si hay mas archivos por procesar cuando leo aprovecho y mando
                     if (files_processed < argc - 1) {
                         ssize_t bytes_written = write(pipe_to_child[i][1], argv[files_processed + 1], strlen(argv[files_processed + 1]) + 1);
-                        //standard issue chequeo de error
                         if (bytes_written == -1) {
+                            if (errno == EPIPE) {
+                                // Handle broken pipe error
+                                fprintf(stderr, "Broken pipe detected\n");
+                                continue;
+                            }
                             perror("write");
                             exit(EXIT_FAILURE);
                         }
                         files_processed++;
                     }
-                } else if (bytes_read == -1) { //standard issue chequeo de error
+                } else if (bytes_read == 0) {
+                    // EOF or no data available
+                    continue;
+                } else {
                     perror("read");
                     exit(EXIT_FAILURE);
                 }
             }
         }
-
     }
 
-    //cierro el archivo results y standard issue chequeo de error
+    // Close all remaining pipes
+    for (int i = 0; i < slaves; i++) {
+        close(pipe_to_child[i][1]);
+        close(pipe_from_child[i][0]);
+    }
+
     if (fclose(file) != 0) {
         perror("Error closing file");
         return EXIT_FAILURE;
     }
-
-
-
-
+    return 0;
 }
-    
-
-
-    
-
--- slave.c
+++ slave.c
@@ -3,6 +3,19 @@
 #define MAX_PATH_SIZE 100
 #define MAX_MD5_SIZE 100
 
+// int pipe_read(int fd, char * buffer){
+//    int i = 0;
+//    char last_read[1];
+//    last_read[0] = 1;
+//    while(last_read[0] != 0 && last_read[0] != '\n' && read(fd, last_read, 1) > 0){
+//       buffer[i++] = last_read[0];
+//    }
+//    buffer[i] = 0;
+//    return i;
+// }
+//esto lo agrego post porque necesito una condicion para que el while corte
+
+
 int main() {
     
     char path[MAX_PATH_SIZE] = {0};
@@ -10,47 +23,49 @@ int main() {
     char * md5sum = "md5sum %s";
     char md5[MAX_MD5_SIZE];
 
-    while(1){
+    while(pipe_read(STDIN_FILENO, path) > 0){       
+        printf("gay\n");     
+        // lectura del pipe
+        if (fgets(path, MAX_PATH_SIZE, stdin) == NULL) {
+                // revisa si no hay nada mas que leer o error
+                continue;
+        }
 
-    // lectura del pipe
-    if (fgets(path, MAX_PATH_SIZE, stdin) == NULL) {
-            // revisa si no hay nada mas que leer o error
-            continue;
-    }
 
+        // saco el newline y lo reemplazo por null terminated
+        path[strcspn(path, "\n")] = '\0';
+
+
+        //Guardo el comando 
+        sprintf(command, md5sum, path);
+        //podria ser asi y se usa el tamano prefijado:
+        //snprintf(command, MAX_CMD_SIZE, md5sum, path);
+
+        //Ejecuto el comando en la shell
+        FILE * log = popen(command, "r");
+
+        if(log == NULL){
+            perror("peopen");
+            exit(EXIT_FAILURE);
+        }
+
+
+
+        //leo md5 checksum
+        if (fgets(md5, MAX_MD5_SIZE, log) != NULL) {
+                // Null-terminate the md5 string properly
+                md5[strcspn(md5, "\n")] = '\0';
+        }
+
+        //Cierro
+        pclose(log);
+
+        //Escribo en el pipe 
+        write(STDOUT_FILENO, md5, strlen(md5) + 1);
+
+        // fflush(stdout); // basicamente las funciones com printf y demas usan un buffer y esta maravilla acelera y asegura el pasaje de datos al stream
     
-    // saco el newline y lo reemplazo por null terminated
-    path[strcspn(path, "\n")] = '\0';
-
-    
-    //Guardo el comando 
-    sprintf(command, md5sum, path);
-    //podria ser asi y se usa el tamano prefijado:
-    //snprintf(command, MAX_CMD_SIZE, md5sum, path);
-
-    //Ejecuto el comando en la shell
-    FILE * log = popen(command, "r");
-
-    if(log == NULL){
-        perror("peopen");
-        exit(EXIT_FAILURE);
     }
+    close(STDOUT_FILENO);
 
-
-
-    //leo md5 checksum
-    if (fgets(md5, MAX_MD5_SIZE, log) != NULL) {
-            // Null-terminate the md5 string properly
-            md5[strcspn(md5, "\n")] = '\0';
-    }
-
-    //Cierro
-    pclose(log);
-
-    //Escribo en el pipe 
-    printf("%s\n", md5);
-
-    fflush(stdout); // basicamente las funciones com printf y demas usan un buffer y esta maravilla acelera y asegura el pasaje de datos al stream
-
-    }
 }
