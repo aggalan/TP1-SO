05ad2897fd5632c71c74dca2b7dcfee062441ea6
Jose Benegas Lynch <jobenegaslynch@itba.edu.ar>
2024-08-28 12:22:41 -0300

    fixed MAJOR things

--- md5.c
+++ md5.c
@@ -1,145 +1,156 @@
 #include "commons.h"
 #include <sys/wait.h>
 #include <sys/select.h>
-#include <errno.h>
 
 #define FILES_PER_SLAVE 1
 
-int main(int argc, const char *argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[]) {
+    // int slaves = ((argc - 1) / 20) > 0? (argc-1)/20:2;
+    int slaves = 1;
+    
+    if(argc < 2){
         fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    int slaves = ((argc - 1) / 20) > 0 ? (argc - 1) / 20 : 1;
-    int files_processed = 0;
+    int files_processed = 0, files_read = 0;
     int max_fd = 0;
 
-    const char *filename = "results.txt";
-    FILE *file = fopen(filename, "w");
+    const char * filename = "results.txt";
+    FILE * file = fopen(filename, "w");
 
     if (file == NULL) {
         perror("Error opening file");
         return EXIT_FAILURE;
     }
 
-    int pipe_to_child[slaves][2], pipe_from_child[slaves][2];
+    // char buff[100];
+    // char * buff1 = "commons.h";
+
+
+    // tengo que leer y escribir con los hijos asi que creo dos pipes
+    int pipe_to_child[slaves][2], pipe_from_child[slaves][2]; 
+
     pid_t pids[slaves];
 
-    for (int i = 0; i < slaves; i++) {
+
+    for (int i = 0; i < slaves; i++) { 
         if (pipe(pipe_to_child[i]) == -1 || pipe(pipe_from_child[i]) == -1) {
             perror("pipe");
             exit(EXIT_FAILURE);
         }
+    }
+
+    for (int i = 0; i< slaves; i++) {
 
         if ((pids[i] = fork()) == 0) {
-            // Child process
+            //proceso hijo
             close(pipe_to_child[i][1]);
             close(pipe_from_child[i][0]);
 
+            //redirecciono las entradas y salidas correspondiente de los pipes
             dup2(pipe_to_child[i][0], STDIN_FILENO);
             close(pipe_to_child[i][0]);
+            
 
-            //dup2(pipe_from_child[i][1], STDOUT_FILENO);
+            dup2(pipe_from_child[i][1], STDOUT_FILENO);
             close(pipe_from_child[i][1]);
 
             char *args[] = {"./slave", NULL};
             execve(args[0], args, NULL);
             perror("execve");
             exit(EXIT_FAILURE);
-        }
-        // Parent process
-       close(pipe_to_child[i][0]); // Close read end
 
-       
-        if (pipe_to_child[i][1] > max_fd) {
-            max_fd = pipe_to_child[i][1];
         }
-        if (pipe_from_child[i][0] > max_fd) {
-            max_fd = pipe_from_child[i][0];
+        //proceso padre
+        close(pipe_to_child[i][0]); //cierro el extremo de lectura
+        close(pipe_from_child[i][1]); //cierro el extremo de escritura 
+    }
+
+    fd_set read_fds, write_fds; //armo mis 2 fd sets para el select (uno para cada pipe)
+
+
+
+    for (int i = 0; i < slaves; i++) {
+        for (int j = 0; j < FILES_PER_SLAVE; j++, files_processed++) {
+            if (files_processed < argc - 1) {
+                write(pipe_to_child[i][1], argv[files_processed + 1], strlen(argv[files_processed + 1]) + 1);
+            } else {
+                break;
+            }
         }
     }
 
-    fd_set read_fds, write_fds;
-
-    while (files_processed < argc - 1) {
+    while(files_read < argc - 1) {
+        printf("ACA");
         FD_ZERO(&read_fds);
         FD_ZERO(&write_fds);
 
         for (int i = 0; i < slaves; i++) {
+
             FD_SET(pipe_to_child[i][1], &write_fds);
             FD_SET(pipe_from_child[i][0], &read_fds);
+
+            if (pipe_to_child[i][1] > max_fd) {
+                max_fd = pipe_to_child[i][1];
+            }
+            if (pipe_from_child[i][0] > max_fd) {
+                max_fd = pipe_from_child[i][0];
+            }
+
         }
 
         int activity = select(max_fd + 1, &read_fds, &write_fds, NULL, NULL);
 
-        if (activity < 0) {
+        
+
+        if (activity == -1) {
+            //womp womp
             perror("select");
             exit(EXIT_FAILURE);
-        }
-
-        for (int i = 0; i < slaves; i++) {
-            if (FD_ISSET(pipe_to_child[i][1], &write_fds)) {
-                if (files_processed < argc - 1) {
-                    ssize_t bytes_written = write(pipe_to_child[i][1], argv[files_processed + 1], strlen(argv[files_processed + 1]) + 1);
-                    if (bytes_written == -1) {
-                        if (errno == EPIPE) {
-                            // Handle broken pipe error
-                            fprintf(stderr, "Broken pipe detected\n");
-                            continue;
-                        }
-                        perror("write");
-                        exit(EXIT_FAILURE);
-                    }
-                    files_processed++;
+        } else if (activity > 0) {
+            //hay algun archivo listo para recibir o leer
+            for (int i = 0; i < slaves; i++) {
+                if (FD_ISSET(pipe_from_child[i][0], &read_fds)) {
+                    //hay algo para leer
+                    char buff[100];
+                    read(pipe_from_child[i][0], buff,100);
+                    printf("el puto buffer dice: %s\n", buff);
+                    files_read++;
                 }
             }
 
-            if (FD_ISSET(pipe_from_child[i][0], &read_fds)) {
-                char buffer[256];
-                ssize_t bytes_read = read(pipe_from_child[i][0], buffer, sizeof(buffer) - 1);
-                if (bytes_read > 0) {
-                    // Ensure null-termination
-                    buffer[bytes_read] = '\0';
-                    if (fprintf(file, "%s\n", buffer) < 0) {
-                        perror("Error writing to file");
-                        fclose(file);
-                        exit(EXIT_FAILURE);
-                    }
 
-                    if (files_processed < argc - 1) {
-                        ssize_t bytes_written = write(pipe_to_child[i][1], argv[files_processed + 1], strlen(argv[files_processed + 1]) + 1);
-                        if (bytes_written == -1) {
-                            if (errno == EPIPE) {
-                                // Handle broken pipe error
-                                fprintf(stderr, "Broken pipe detected\n");
-                                continue;
-                            }
-                            perror("write");
-                            exit(EXIT_FAILURE);
-                        }
-                        files_processed++;
-                    }
-                } else if (bytes_read == 0) {
-                    // EOF or no data available
-                    continue;
-                } else {
-                    perror("read");
-                    exit(EXIT_FAILURE);
-                }
-            }
         }
+
+
+
+
     }
 
-    // Close all remaining pipes
-    for (int i = 0; i < slaves; i++) {
-        close(pipe_to_child[i][1]);
-        close(pipe_from_child[i][0]);
-    }
+
+    
+
+    // printf("ACA");
+
+    // write(pipe_to_child[0][1], buff1, strlen(buff1) + 1);
+
+    // printf("ACA1");
+
+    // wait(NULL);
+
+    // printf("ACA2\n");
+
+    // read(pipe_from_child[0][0], buff, 100);
+
+    // printf("el buffer dice: %s\n", buff);
 
     if (fclose(file) != 0) {
         perror("Error closing file");
         return EXIT_FAILURE;
     }
+
     return 0;
+
 }
+   
--- slave.c
+++ slave.c
@@ -3,19 +3,6 @@
 #define MAX_PATH_SIZE 100
 #define MAX_MD5_SIZE 100
 
-int pipe_read(int fd, char * buffer){
-   int i = 0;
-   char last_read[1];
-   last_read[0] = 1;
-   while(last_read[0] != 0 && last_read[0] != '\n' && read(fd, last_read, 1) > 0){
-      buffer[i++] = last_read[0];
-   }
-   buffer[i] = 0;
-   return i;
-}
-//esto lo agrego post porque necesito una condicion para que el while corte
-
-
 int main() {
     
     char path[MAX_PATH_SIZE] = {0};
@@ -23,16 +10,24 @@ int main() {
     char * md5sum = "md5sum %s";
     char md5[MAX_MD5_SIZE];
 
-    while(pipe_read(STDIN_FILENO, path) > 0){       
+    // while (1) {
+    //     if (read(STDIN_FILENO, path, 10)) {
+    //         write(STDOUT_FILENO, path, 10);
+    //         break;
+    //     }
+    // }
 
+    
+
+
+    while(1){
+        
         // lectura del pipe
-        if (fgets(path, MAX_PATH_SIZE, stdin) == NULL) {
+        if (read(STDIN_FILENO, path, MAX_PATH_SIZE) <= 0) {
                 // revisa si no hay nada mas que leer o error
                 continue;
-        }
-
-
-        // saco el newline y lo reemplazo por null terminated
+        } else {
+            // saco el newline y lo reemplazo por null terminated
         path[strcspn(path, "\n")] = '\0';
 
 
@@ -52,10 +47,8 @@ int main() {
 
 
         //leo md5 checksum
-        if (fgets(md5, MAX_MD5_SIZE, log) != NULL) {
-                // Null-terminate the md5 string properly
-                md5[strcspn(md5, "\n")] = '\0';
-        }
+        fgets(md5, MAX_MD5_SIZE, log);
+        
 
         //Cierro
         pclose(log);
@@ -63,9 +56,13 @@ int main() {
         //Escribo en el pipe 
         write(STDOUT_FILENO, md5, strlen(md5) + 1);
 
-        // fflush(stdout); // basicamente las funciones com printf y demas usan un buffer y esta maravilla acelera y asegura el pasaje de datos al stream
+        fflush(stdout); // basicamente las funciones com printf y demas usan un buffer y esta maravilla acelera y asegura el pasaje de datos al stream
+        
+        
+        }
+
+
+        
     
     }
-    close(STDOUT_FILENO);
-
 }
